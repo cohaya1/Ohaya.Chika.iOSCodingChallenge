3631652839f042e4ebf892e0c430fcd42a6d51e6
cohaya1 <chika.ohaya@gmail.com>
2020-08-15 15:45:03 -0400

    Initial Commit

---
+++ Ohaya.Chika.iOSCodingChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ohaya.Chika.iOSCodingChallenge
+//
+//  Created by Makaveli Ohaya on 8/15/20.
+//  Copyright © 2020 Ohaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
